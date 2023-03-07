fdf81a21696277579fa9ccd38884d7e1c18d1edb
yydAble <296821850@qq.com>
2023-03-07 22:23:38 +0800

    submit vue-drag

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <Drag :componentMap="componentMap" :layoutList="layoutList" @change="onChange"></Drag>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Drag from './components/Drag'
+import Mod1 from './modules/mod-1.vue'
+import Mod2 from './modules/mod-2.vue'
+import Mod3 from './modules/mod-3.vue'
+
+const componentMap = {
+  Mod1: Mod1,
+  Mod2: Mod2,
+  Mod3: Mod3
+}
+
+const layoutList = [
+  {
+    name:'组件001',
+    componentName:'Mod1'
+  },
+  {
+    name:'组件002',
+    componentName:'Mod2'
+  },
+  {
+    name:'组件003',
+    componentName:'Mod3'
+  },
+]
+
+const onChange = (e:unknown)=>{
+  console.log('onChange===',e)
+}
+</script>
+
+<style scoped>
+
+</style>
